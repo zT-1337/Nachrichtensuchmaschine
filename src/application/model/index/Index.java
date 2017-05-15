41d8230df6ecd5f7bd6d4c5cf13981d847399550
Gnom97 <thomas.zerr@googlemail.com>
2017-05-15 12:07:57 +0200

    Defined search and index interfaces

--- src/application/model/index/Index.java
+++ src/application/model/index/Index.java
@@ -1,5 +1,11 @@
 package application.model.index;
 
+import org.apache.lucene.document.Document;
+import org.apache.lucene.search.Query;
+
 public interface Index {
 
+	public void addNews(Document doc);
+	
+	public Document[] searchFor(Query querry, int n);
 }
---
+++ src/application/model/index/LuceneIndex.java
@@ -0,0 +1,20 @@
+package application.model.index;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.search.Query;
+
+public class LuceneIndex implements Index {
+
+	@Override
+	public void addNews(Document doc) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Document[] searchFor(Query querry, int n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
