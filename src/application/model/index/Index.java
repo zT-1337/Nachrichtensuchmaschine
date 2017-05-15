6e8daefd3023312b251a85c24f62be46a0af9680
Gnom97 <thomas.zerr@googlemail.com>
2017-05-15 17:24:27 +0200

    Changed the return type of the searchFor interface, from Document[] to ScoreDoc[], since the score is also needed

--- src/application/model/index/Index.java
+++ src/application/model/index/Index.java
@@ -1,11 +1,14 @@
 package application.model.index;
 
+import java.io.IOException;
+
 import org.apache.lucene.document.Document;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
 
 public interface Index {
 
-	public void addNews(Document doc);
+	public void addNews(Document doc) throws IOException;
 	
-	public Document[] searchFor(Query querry, int n);
+	public ScoreDoc[] searchFor(Query querry, int n);
 }
