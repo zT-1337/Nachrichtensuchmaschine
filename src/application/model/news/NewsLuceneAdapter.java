d88e3508942b518292e448e6d355b643b2723157
Gnom97 <thomas.zerr@googlemail.com>
2017-05-19 08:57:16 +0200

    Setter und Getter Methoden implementiert - ungetestet, keine Fehlerbehandlung

--- src/application/model/news/NewsLuceneAdapter.java
+++ src/application/model/news/NewsLuceneAdapter.java
@@ -1,6 +1,9 @@
 package application.model.news;
 
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 
 public class NewsLuceneAdapter implements News {
 	
@@ -18,6 +21,12 @@ public class NewsLuceneAdapter implements News {
 	public String getTitle() {
 		// TODO Auto-generated method stub
 		return doc_.get(NewsFields.TITLE);
+	}
+	
+	@Override
+	public String getTopic() {
+		// TODO Auto-generated method stub
+		return doc_.get(NewsFields.TOPIC);
 	}
 
 	@Override
@@ -53,37 +62,51 @@ public class NewsLuceneAdapter implements News {
 	@Override
 	public void setTitle(String value) {
 		// TODO Auto-generated method stub
-
+		TextField titel = new TextField(NewsFields.TITLE, value, Field.Store.YES);
+		doc_.add(titel);
+	}
+	
+	@Override
+	public void setTopic(String value) {
+		// TODO Auto-generated method stub
+		StringField topic = new StringField(NewsFields.TOPIC, value, Field.Store.YES);
+		doc_.add(topic);
 	}
 
 	@Override
 	public void setPubDate(String value) {
 		// TODO Auto-generated method stub
-
+		StringField pubdate = new StringField(NewsFields.PUBDATE, value, Field.Store.YES);
+		doc_.add(pubdate);
 	}
 
 	@Override
 	public void setSource(String value) {
 		// TODO Auto-generated method stub
-
+		//doc_.removeField(NewsFields.SOURCE);
+		StringField source = new StringField(NewsFields.SOURCE, value, Field.Store.YES);
+		doc_.add(source);
 	}
 
 	@Override
 	public void setURL(String value) {
 		// TODO Auto-generated method stub
-
+		StringField url = new StringField(NewsFields.URL, value, Field.Store.YES);
+		doc_.add(url);
 	}
 
 	@Override
 	public void setText(String value) {
 		// TODO Auto-generated method stub
-
+		TextField text = new TextField(NewsFields.TEXT, value, Field.Store.NO);
+		doc_.add(text);
 	}
 
 	@Override
 	public void setReducedText(String value) {
 		// TODO Auto-generated method stub
-
+		TextField reducedText = new TextField(NewsFields.REDUCEDTEXT, value, Field.Store.NO);
+		doc_.add(reducedText);
 	}
 
 	@Override
