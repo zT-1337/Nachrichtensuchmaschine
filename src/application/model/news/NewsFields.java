e74df9a9fb3b40c39a92ef1cb1203bfd4fbf6362
Gnom97 <thomas.zerr@googlemail.com>
2017-05-19 08:38:57 +0200

    Init NewsFields

---
+++ src/application/model/news/NewsFields.java
@@ -0,0 +1,12 @@
+package application.model.news;
+
+public class NewsFields {
+
+	public static final String TITLE = "title";
+	public static final String PUBDATE = "pubdate";
+	public static final String SOURCE = "source";
+	public static final String URL = "url";
+	public static final String TEXT = "text";
+	public static final String REDUCEDTEXT = "reducedtext";
+	
+}
