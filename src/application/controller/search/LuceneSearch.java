88115d4f09a94ae317842ab2b10ca1c349370b14
Gnom97 <thomas.zerr@googlemail.com>
2017-05-29 15:27:18 +0200

    QueryCreator hinzugefügt

--- src/application/controller/search/LuceneSearch.java
+++ src/application/controller/search/LuceneSearch.java
@@ -2,11 +2,10 @@ package application.controller.search;
 
 import java.util.StringTokenizer;
 
-import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanClause.Occur;
 import org.apache.lucene.search.BooleanQuery.Builder;
-import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.Query;
 
 import application.model.index.Index;
 import application.model.news.NewsFields;
@@ -17,9 +16,14 @@ public class LuceneSearch implements Search {
 	private final static int minimum = 1;
 
 	private Index index_;
+	private QueryCreator termCreator;
+	private QueryCreator dateCreator;
 	
 	public LuceneSearch(Index index) {
 		index_ = index;
+		
+		termCreator = new TermCreator();
+		dateCreator = new DateCreator();
 	}
 	
 	@Override
@@ -54,40 +58,42 @@ public class LuceneSearch implements Search {
 		else
 			occur = Occur.MUST;
 
-		return createBooleanClause(terms, NewsFields.TEXT, occur, occur);
+		return createBooleanClause(terms, NewsFields.TEXT, termCreator, occur, occur);
 	}
 	
 	private BooleanClause createDatesClause(String dates) {
-		return null;
+		Occur occurBoolean = Occur.FILTER;
+		Occur occurTerm = Occur.SHOULD;
+		
+		return createBooleanClause(dates, NewsFields.PUBDATE, dateCreator, occurBoolean, occurTerm);
 	}
 	
 	private BooleanClause createTopicsClause(String topics) {
 		Occur occurBoolean = Occur.FILTER;
 		Occur occurTerm = Occur.SHOULD;
 		
-		return createBooleanClause(topics, NewsFields.TOPIC, occurBoolean, occurTerm);
+		return createBooleanClause(topics, NewsFields.TOPIC, termCreator, occurBoolean, occurTerm);
 	}
 	
 	private BooleanClause createNewsClause(String news) {
 		Occur occurBoolean = Occur.MUST;
 		Occur occurTerms = Occur.SHOULD;
 		
-		return createBooleanClause(news, NewsFields.REDUCEDTEXT, occurBoolean, occurTerms);
+		return createBooleanClause(news, NewsFields.REDUCEDTEXT, termCreator, occurBoolean, occurTerms);
 		
 	}
 	
-	private BooleanClause createBooleanClause(String str, String field, Occur occurBoolean, Occur occurTerms) {		
+	private BooleanClause createBooleanClause(String str, String field, QueryCreator creator, Occur occurBoolean, Occur occurTerms) {		
 		Builder builder = new Builder();
 		builder.setMinimumNumberShouldMatch(LuceneSearch.minimum);
 		
 		StringTokenizer st = new StringTokenizer(str);
 		
 		while(st.hasMoreTokens()) {
-			Term term = new Term(field, st.nextToken());
-			TermQuery query = new TermQuery(term);
-			
+			Query query = creator.create(field, st.nextToken());
 			builder.add(query, occurTerms);
 		}
+		
 		
 		return new BooleanClause(builder.build(), occurBoolean);
 	}
