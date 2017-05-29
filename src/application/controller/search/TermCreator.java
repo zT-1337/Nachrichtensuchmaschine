3ed8abc3458c49aff8bd7afdd982d8676191f1ce
Gnom97 <thomas.zerr@googlemail.com>
2017-05-29 16:24:58 +0200

    Javadoc angelegt

--- src/application/controller/search/TermCreator.java
+++ src/application/controller/search/TermCreator.java
@@ -1,11 +1,28 @@
+/*
+ * TermCreator
+ * 
+ * Version: 1.0
+ * 
+ * Datum: 29.05.2017
+ */
 package application.controller.search;
 
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 
+/**
+ * Erzeugt eine TermQuery.
+ * 
+ * @author Thomas Zerr
+ * @version 1.0
+ *
+ */
 public class TermCreator implements QueryCreator {
 
+	/**
+	 * @param value Der Begriff aus dem eine TermQuery erzeugt wird
+	 */
 	@Override
 	public Query create(String field, String value) {
 		// TODO Auto-generated method stub
