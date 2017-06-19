1624e094132b09f24e9b10aea7ef88f58c4ebb3c
Gnom97 <thomas.zerr@googlemail.com>
2017-06-19 17:01:20 +0200

    Testfälle geschrieben

--- src/test/controller/search/TermCreatorTest.java
+++ src/test/controller/search/TermCreatorTest.java
@@ -2,13 +2,90 @@ package test.controller.search;
 
 import static org.junit.Assert.*;
 
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
 import org.junit.Test;
+
+import application.controller.search.TermCreator;
 
 public class TermCreatorTest {
 
+	private TermCreator creator_ = new TermCreator();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void EmptyField() {
+		String field = "";
+		String value = "Term";
+		
+		Query result = creator_.create(field, value);
+		
+		assertNull(result);
+	}
+	
+	@Test
+	public void EmptyValue() {
+		String field = "field";
+		String value = "";
+		
+		Query result = creator_.create(field, value);
+		
+		assertNull(result);
+	}
+	
+	@Test
+	public void EmptyParams() {
+		String field = "";
+		String value = "";
+		
+		Query result = creator_.create(field, value);
+		
+		assertNull(result);
+	}
+	
+	@Test
+	public void NullField() {
+		String field = null;
+		String value = "Term";
+		
+		Query result = creator_.create(field, value);
+		
+		assertNull(result);
+	}
+	
+	@Test
+	public void NullValue() {
+		String field = "field";
+		String value = null;
+		
+		Query result = creator_.create(field, value);
+		
+		assertNull(result);
+	}
+	
+	@Test
+	public void NullParams() {
+		String field = null;
+		String value = null;
+		
+		Query result = creator_.create(field, value);
+		
+		assertNull(result);
+	}
+	
+	@Test
+	public void BothParamsValid() {
+		String field = "field";
+		String value = "Term";
+		
+		Query result = creator_.create(field, value);
+		
+		assertTrue(result instanceof TermQuery);
+		
+		TermQuery queryResult = (TermQuery) result;
+		
+		assertTrue(queryResult.getTerm().text().equals(value));
+		assertTrue(queryResult.getTerm().field().equals(field));
+		
 	}
 
 }
