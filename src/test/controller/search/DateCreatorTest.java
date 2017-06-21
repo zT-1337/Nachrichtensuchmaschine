b93587db09f74954f618cd5141df37afc7295111
Gnom97 <thomas.zerr@googlemail.com>
2017-06-21 17:01:42 +0200

    Testfälle neu aufgebaut

--- src/test/controller/search/DateCreatorTest.java
+++ src/test/controller/search/DateCreatorTest.java
@@ -6,111 +6,54 @@ import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TermRangeQuery;
 import org.apache.lucene.util.BytesRef;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import application.controller.search.DateCreator;
+import application.model.news.NewsFields;
 
 public class DateCreatorTest {
 	
-	private DateCreator creator = new DateCreator();
+	private DateCreator creator;
 	
-	private String field = "Field";
-	private String fieldEmpty = "";
-	private String fieldNull = null;
-	
-	private String dateEmpty = "";
-	private String dateNull = null;
-	
-	private String oneDate = "01.06.1997";
-	private String secondDate = "02.06.1997";
-	
-	private String oneDateFalseFormat[] = new String[]{"1.6.97", "01.6.97", "1.06.97", "1.6.1997", 
-														"o1.o6.o97", "o1.o6.97", "1a.2b.199c",
-														"01-06-1997", "1-06-1997", "01-6-1997", "01-06-197", 
-														"01/06/1997", "1/06/1997", "01/6/1997", "01/06/997",
-														"01.06/1997", "01/06.1997", "01-06/1997", "01/06-1997",
-														"01.06-1997", "01-06.1997"};
-	
-	
-	
-	private String twoDates = oneDate + "-" + secondDate;
-	private String falseDateSeperators[] = new String[]{".", " ", "/"};
-
-	@Test
-	public void OneDateFalseFormat() {
-		for(int i = 0; i < oneDateFalseFormat.length; ++i)
-			assertNull("Testfall: " + oneDateFalseFormat[i], creator.create(field, oneDateFalseFormat[i]));
+	@Before
+	public void setUp() {
+		creator = new DateCreator();
 	}
 	
-	@Test
-	public void TwoDatesFalseFormat() {
-		for(int i = 0; i < falseDateSeperators.length; ++i) {
-			String temp = oneDate + falseDateSeperators[i] + oneDate;
-			
-			assertNull("Testfall: " + temp, creator.create(field, temp));
-		}
-	}
-	
-	@Test
-	public void DateEmpty() {
-		assertNull(creator.create(field, dateEmpty));
-	}
-	
-	@Test
-	public void DateNull() {
-		assertNull(creator.create(field, dateNull));
-	}
-	
-	@Test
-	public void FieldEmpty() {
-		assertNull(creator.create(fieldEmpty, oneDate));
-		assertNull(creator.create(fieldEmpty, twoDates));
-	}
-	
-	@Test
-	public void FieldNull() {
-		assertNull(creator.create(fieldNull, oneDate));
-		assertNull(creator.create(fieldNull, twoDates));
-	}
-	
-	@Test
-	public void ParamsEmpty() {
-		assertNull(creator.create(fieldEmpty, dateEmpty));
-	}
-	
-	@Test
-	public void ParamsNull() {
-		assertNull(creator.create(fieldNull, dateNull));
-	}
-	
-	@Test
-	public void ParamsValidOneDate() {
-		Query result = creator.create(field, oneDate);
+	@After
+	public void tearDown() {
 		
-		assertNotNull(result);
-		assertTrue(result instanceof TermQuery);
-		
-		TermQuery termQuery = (TermQuery) result;
-		
-		assertTrue(termQuery.getTerm().text().equals(oneDate));
-		assertTrue(termQuery.getTerm().field().equals(field));
 	}
 	
 	@Test
-	public void ParamsValidTwoDates() {
-		Query result = creator.create(field, twoDates);
+	public void testOneValidDate() {
+		String field = NewsFields.PUBDATE;
+		String date;
+		Query query;
 		
-		assertNotNull(result);
-		assertTrue(result instanceof TermRangeQuery);
+		date = "01.06.1997";
+		query = creator.create(field, date);
+		assertTrue(query instanceof TermQuery);
+		TermQuery termQuery = (TermQuery) query;
+		assertTrue(termQuery.getTerm().text().equals("19970601"));
 		
-		TermRangeQuery termRangeQuery = (TermRangeQuery) result;
-		BytesRef lowerTerm = new BytesRef(oneDate);
-		BytesRef upperTerm = new BytesRef(secondDate);
+	}
+	
+	@Test
+	public void testTwoValidDates() {
+		String field = NewsFields.PUBDATE;
+		String dates;
+		Query query;
 		
-		assertTrue(termRangeQuery.includesLower());
-		assertTrue(termRangeQuery.includesUpper());
-		assertTrue(termRangeQuery.getLowerTerm().equals(lowerTerm));
-		assertTrue(termRangeQuery.getUpperTerm().equals(upperTerm));
+		dates = "01.01.1997-02.06.2000";
+		query = creator.create(field, dates);
+		assertTrue(query instanceof TermRangeQuery);
+		TermRangeQuery termRangeQuery = (TermRangeQuery) query;
+		assertTrue(termRangeQuery.getLowerTerm().equals(new BytesRef("19970101")));
+		assertTrue(termRangeQuery.getUpperTerm().equals(new BytesRef("20000602")));
+		
 	}
 
 }
