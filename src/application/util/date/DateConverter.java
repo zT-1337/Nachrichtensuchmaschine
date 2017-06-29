6a380ff18c245a62e497252623e8d689e7f9847c
Gnom97 <thomas.zerr@googlemail.com>
2017-06-29 16:03:25 +0200

    numberToDate Methode implementiert

--- src/application/util/date/DateConverter.java
+++ src/application/util/date/DateConverter.java
@@ -34,11 +34,29 @@ public class DateConverter {
 	 * @return Das konvertierte Datum oder null, falls das übergebene Datum, nicht im richtigen Format war.
 	 */
 	public String dateToNumber(String date) {
+		//Validierung des Parameters
 		if(date == null || !matcher.oneDateMatches(date))
 			return null;
 		
+		//Umwandlung von dd.mm.yyyy zu yyyymmdd
 		String result = date.substring(6, 10) + date.substring(3, 5) + date.substring(0, 2);
 		
+		
+		return result;
+	}
+	
+	/**
+	 * Konvertiert eine Zahl mit 8 stellen in Datum. (Format dd.mm.yyyy)
+	 * @param number Die zu konvertierende Zahl
+	 * @return Die konvertierte Zahl oder null, falls die Zahl zu kurz war.
+	 */
+	public String numberToDate(String number) {
+		//Validierung des Parameters
+		if(number == null || number.length() != 8)
+			return null;
+		
+		//Umwandlung von yyyymmdd zu dd.mm.yyyy
+		String result = number.substring(6, 8) + "." + number.substring(4, 6) + "." + number.substring(0, 4);
 		
 		return result;
 	}
